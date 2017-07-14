767ec01c329753efc670b2a64f49c4d7df00ab65
pavel.yeliseev <jelisejev@users.noreply.github.com>
2017-07-14 21:58:24 +0300

    add filter

--- src/App.js
+++ src/App.js
@@ -2,18 +2,38 @@ import React, { Component } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import AppBar from 'material-ui/AppBar';
 import MovieList from './MovieList';
+import TextField from 'material-ui/TextField';
 import './App.css';
 
+import allMovies from './movies.json';
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      movies: allMovies
+    };
+  }
+
   render() {
     return (
       <MuiThemeProvider>
         <div>
           <AppBar title="React Movie Data Base" />
-          <MovieList/>
+          <p style={{paddingLeft: 20}}>
+            Search: <TextField name="search" onChange={this.onSearch} />
+          </p>
+          <MovieList movies={this.state.movies} />
         </div>
       </MuiThemeProvider>
     );
+  }
+
+  onSearch = (event, query) => {
+    const movies = allMovies.filter(movie => movie.Title.toLowerCase().indexOf(query.toLowerCase()) !== -1);
+    this.setState({
+      movies
+    });
   }
 }
 
--- src/MovieList.jsx
+++ src/MovieList.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import {GridList, GridTile} from 'material-ui/GridList';
 
-import movies from './movies.json';
-
-const MovieList = () => (
+const MovieList = ({ movies }) => (
   <GridList cols="4" cellHeight="auto">
     {movies.map(movie => (
       <GridTile key={movie.imdbID} title={movie.Title} subtitle={movie.Year}>
